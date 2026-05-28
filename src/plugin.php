c3965a3db74377b63e79c334dd1cb71980b4a53f
Vega Nicholas S. <veganicholas123@gmail.com>
2026-05-29 04:34:02 +0700

    chore: disable dev mode

--- src/plugin.php
+++ src/plugin.php
@@ -42,7 +42,7 @@ use function dirname;
 class LittleBrother extends PluginBase{
 	use SingletonTrait;
 
-	public const IS_DEVELOPMENT = true;
+	public const IS_DEVELOPMENT = false;
 
 	private ProtocolStorage $protocolStorage;
 	private PacketTranslator $translator;
